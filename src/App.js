72dd180eb9de2df05b7c2321ede1fbb9f4aaaa7f
cisse410 <dramisse90@gmail.com>
2024-06-02 18:16:21 +0000

    Consommation d'un API Laravel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.css";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+
+
+import ListProduct from "./compnents/product/list.component";
+import CreateProduct from "./compnents/product/create.component";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar bg="primary">
+        <Container>
+          <Link className="navbar-brand text-white">
+            Application CRUD de gestion de produits
+          </Link>
+        </Container>
+      </Navbar>
+      <Container className="mt-5">
+        <Row>
+          <Col md={12}>
+            <Routes>
+              <Route exact path="/" element={<ListProduct/>}/>
+              <Route path="product/create" element={<CreateProduct/>}/>
+            </Routes>
+          </Col>
+        </Row>
+      </Container>
+    </Router>
   );
 }
 
